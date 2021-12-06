bbf3c676eadd314fb844d2f80d19e59b9b748f71
Aizengr <capacapass@gmail.com>
2021-12-07 00:04:44 +0200

    boom

--- myscript.js
+++ myscript.js
@@ -1,27 +1,53 @@
- function myValidity(){
+let pass1 = document.getElementById("pass1")
+let pass2 = document.getElementById("pass2")
+let form = document.getElementById("registration-form")
 
-    
-    let form = document.getElementById('form');
-    /*password validation*/
-    let elem1 = document.getElementById("pass1");
-    let elem2 = document.getElementById("pass2");
-    console.log(elem1.value);
-    console.log(elem2.value);
+form.addEventListener("submit", (e) => { /*event listener gia thn forma*/
+    if (pass1.value!=pass2.value){
+        e.preventDefault();
+        pass1.style.borderColor="red" /*kokkino border*/
+        pass2.style.borderColor="red"
+        let list = document.querySelectorAll(".password-error") /*epilegw ola ta elements me auth th klash*/
+        for(let item of list) {
+            item.textContent = "Passwords do not match. Please try again!" /*vazw text giat to error message se kathe section me th sugekrimenh klash*/
+        }
+    }
 
-    if(elem1.value!=elem2.value){
- 
-       elem1.style.borderColor="red";
-       elem2.style.borderColor="red";
-       elem1.setCustomValidity("Password does not match");
-       return false;
+})
+
+let date = document.getElementById("birthdate")
+date.addEventListener("input", (e) => { 
+    result = calculate_age(date)
+    if (result <= 12) {
+        e.preventDefault();
+        date.setCustomValidity("You need to be above 12 years old in order to complete the registration.")
+        date.style.borderColor="red"
     }
     else{
-        elem1.setCustomValidity("");
-        return true;
+        date.setCustomValidity("") /*resetting validity*/
     }
-    
+})
 
-    
+let email = document.getElementById("email")
+email.addEventListener(("input"), (e) => {
+    let re= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    if(!re.test(email.value)){
+        e.preventDefault();
+        email.setCustomValidity("Email needs to be under the following format: yourname@domain.tld")
+        email.style.borderColor="red"
+    }
+    else{
+        email.setCustomValidity("")
+    }
+})
 
+
+ 
+
+function calculate_age(date) { /*sunarthsh olodikia mas gia ton upologismo ths hlikias*/
+    newd = new Date (date.value)
+    var diff_ms = Date.now() - newd.getTime();
+    var age_dt = new Date(diff_ms); 
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
 }
-
